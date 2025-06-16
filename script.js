7dce16e1c15f471b366dfe7b7da8c2809fccb66c
Max <146020430+maxxi0030@users.noreply.github.com>
2025-06-16 07:35:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+// Temas
+const btn = document.getElementById("theme-toggle");
+btn.onclick = () => {
+  document.body.classList.toggle("dark");
+  localStorage.setItem("theme", document.body.classList.contains("dark") ? "dark" : "light");
+};
+
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark");
+}
+
+// apskates
+let hits = localStorage.getItem("hits") || 0;
+hits++;
+localStorage.setItem("hits", hits);
+document.getElementById("hit-counter").textContent = hits;
+
+// Gads
+document.getElementById("year").textContent = new Date().getFullYear();
 
